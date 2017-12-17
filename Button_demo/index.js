3590cead82548718f9c2e608c32e91fe28be7d92
H008Shah <hardikshah008@gmail.com>
2017-12-17 18:28:55 +0530

    ReactNativeButton
    How to Add button in react native and get click event

---
+++ Button_demo/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import button_demo from './src/button_demo';
+
+
+AppRegistry.registerComponent('Button_demo', () => button_demo);
